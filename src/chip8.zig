8429de726e76be92756e396286f18451a4a9e86c
Denkkou <JoeSchoDev@gmail.com>
2025-12-11 18:34:47 +0200

    Started building gfx byte array to image, and image to texture. Currently broken. :P

---
+++ src/chip8.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+
+pub const CPU = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+
+    opcode: u16,
+    memory: []u8,
+    gfx: []u8,
+    V: []u8, // V0 through VF registers
+    I: u16, // Index register
+    pc: u16,
+    delay_timer: u8,
+    sound_timer: u8,
+    stack: []u16,
+    stack_pointer: u8,
+    keys: []u8,
+    should_draw: u8,
+
+    pub fn create(allocator: std.mem.Allocator) !Self {
+        const memory = try allocator.alloc(u8, 4096);
+        const gfx = try allocator.alloc(u8, 2048);
+        const v_reg = try allocator.alloc(u8, 16);
+        const stack = try allocator.alloc(u16, 16);
+        const keys = try allocator.alloc(u8, 16);
+
+        // Zero-initialise all of the memory regions
+        @memset(memory, 0);
+        @memset(gfx, 1);
+        @memset(v_reg, 0);
+        @memset(stack, 0);
+        @memset(keys, 0);
+
+        // Fully initialise registers
+        return Self{
+            .allocator = allocator,
+            .opcode = 0,
+            .memory = memory,
+            .gfx = gfx,
+            .V = v_reg,
+            .I = 0,
+            .pc = 0x0200, // Start of program
+            .delay_timer = 0,
+            .sound_timer = 0,
+            .stack = stack,
+            .stack_pointer = 0,
+            .keys = keys,
+            .should_draw = 1, // Default for program start
+        };
+    }
+
+    // Free memory
+    pub fn free(self: *Self) void {
+        self.allocator.free(self.memory);
+        self.allocator.free(self.gfx);
+        self.allocator.free(self.V);
+        self.allocator.free(self.stack);
+        self.allocator.free(self.keys);
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,68 @@
 const std = @import("std");
 const rl = @import("raylib");
+const chip8 = @import("chip8.zig").CPU;
+
+const screen_width: u16 = 640;
+const screen_height: u16 = 320;
 
 pub fn main() !void {
     // Allocator
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    // Catch any potential memory leaks
+    defer {
+        const leaked = gpa.deinit();
+        if (leaked == .leak) {
+            @panic("Memory leak detected!");
+        }
+    }
 
     // Initialise CPU
+    var cpu = try chip8.create(allocator);
+    defer cpu.free();
 
     // Load ROM
 
     // Initialise Raylib
-    rl.initWindow(640, 320, "CHIP-8 Emulator");
+    rl.initWindow(screen_width, screen_height, "CHIP-8 Emulator");
     defer rl.closeWindow();
 
+    var image: rl.Image = undefined;
+    var tex: rl.Texture2D = undefined;
+
     while (!rl.windowShouldClose()) {
+        // Get Input
+        // Cycle once
+
+        // Draw graphics from gfx array
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        // Get Input
+        if (cpu.should_draw == 1) {
+            image = try imageFromGFX(&cpu);
+            tex = try rl.loadTextureFromImage(image);
+            std.debug.print("Drew texture from image", .{});
+        }
 
-        // Perform one emulation cycle
-
-        // Draw gfx array scaled up to window size
+        rl.drawTexture(tex, 0, 0, rl.Color.white);
+        cpu.should_draw = 0;
     }
+}
+
+pub fn imageFromGFX(cpu: *chip8) !rl.Image {
+    // initRaw wants a null terminated file as its first arg
+    // however cpu.gfx is a 2048 byte array of pixel data
+    // not sure how I can make an array that it'll accept :[
+    const img = try rl.Image.initRaw(
+        cpu.gfx, // DOESNT LIKE THIS!
+        64,
+        32,
+        rl.PixelFormat.uncompressed_grayscale,
+        0,
+    );
+
+    return img;
 }
 
 // Raylib needs only to capture input and draw graphics
@@ -40,3 +81,10 @@ pub fn main() !void {
 // for the gfx array, we could either draw rects in accordance with
 // the pixels, or we can build a bitmap? then draw the bitmap to the screen
 // to automatically scale it
+
+// Emulate cycle
+// Fetch opcode
+// Decode opcode
+// Run opcode
+// if shouldDraw = 1
+// update graphics array
